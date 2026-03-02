0f46da0b7593faf6548be3f36124eef6a2769bf2
Vanley John B. Monares <vjsaiken22@gmail.com>
2026-03-03 05:14:24 +0800

    Added Git hub actions deploymet workflow

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -3,7 +3,11 @@ function About() {
     <section className="box">
       <h2>About Me</h2>
 
-      <img src="/images/profile.jpg" alt="Profile photo" width="150" />
+      <img
+        src={`${import.meta.env.BASE_URL}images/profile.jpg`}
+        alt="Profile photo"
+        width="150"
+      />
 
       <p>
         I am a BSIT student at USTP who enjoys building clean and modern web layouts.
